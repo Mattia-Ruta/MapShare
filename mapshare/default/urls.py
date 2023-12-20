12655b7eed0d2c1c45907466050ae463ba48a20c
Mattia Ruta <git@mattiaruta.co.uk>
2023-12-20 21:56:00 +0000

    Initial commit

--- mapshare/default/urls.py
+++ mapshare/default/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:mapcode>", views.index, name="index"),
+    path("<str:context>/<str:mapcode>", views.index, name="index"),
 ]
--- mapshare/default/views.py
+++ mapshare/default/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def index(request, context="", mapcode=""):
     return render(request, "index.html", {})
