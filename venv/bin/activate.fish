5673f3126cd6d1d8d97892ae942ffcfb52465d8b
Mattia Ruta <ruta.mattia@gmail.com>
2024-04-18 00:18:46 +0000

    Add googlemaps package to requirements

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mattia/Documents/MapShare/venv"
+set -gx VIRTUAL_ENV "/var/www/html/mapshare.xyz/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
